5a9f223e7a3686fcc62c5235554d3c9130765864
Arthur Pinheiro <arthurpiinheiro@gmail.com>
2020-04-20 02:21:10 -0300

    Alteracao a nomenclatura do status.

--- src/app/demand-management/demand-excel/demand-excel.component.ts
+++ src/app/demand-management/demand-excel/demand-excel.component.ts
@@ -1,35 +1,33 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {ExcelService} from './demand-excel.service';
-import { DemandService } from '../demand.service';
+import {DemandService} from '../demand.service';
 
 @Component({
-  selector: 'app-demand-excel',
-  templateUrl: './demand-excel.component.html',
-  styleUrls: ['./demand-excel.component.scss']
+    selector: 'app-demand-excel',
+    templateUrl: './demand-excel.component.html',
+    styleUrls: ['./demand-excel.component.scss']
 })
 export class DemandExcelComponent implements OnInit {
-  
-  @Input('demandsFilter') demandsFilter: any;
-  
-  constructor(private excelService: ExcelService, private demandService: DemandService) { }
+
+    @Input('demandsFilter') demandsFilter: any;
+
+    constructor(private excelService: ExcelService, private demandService: DemandService) {
+    }
 
     ngOnInit() {
     }
 
-    ngOnChanges(event) {
-    }
-  
     getDemandsExcel() {
     }
 
-    exportAsXLSX():void {
-      if (this.demandsFilter) {
-        this.demandService.getDemandsExcel(this.demandsFilter).subscribe(res => {
-          if (res && res.data && res.data.length) {
-             this.excelService.exportAsExcelFile(res.data, 'demandas');
-          }
-        });
-      }
+    exportAsXLSX(): void {
+        if (this.demandsFilter) {
+            this.demandService.getDemandsExcel(this.demandsFilter).subscribe(res => {
+                if (res && res.data && res.data.length) {
+                    this.excelService.exportAsExcelFile(res.data, 'demandas');
+                }
+            });
+        }
     }
 
 
